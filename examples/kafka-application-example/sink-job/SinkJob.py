5ec97216e0a89b112f8c1fe6e8d62dfdf3f233bf
Tony Tan <tony@tonytan.io>
2020-11-19 16:45:11 -0500

    test adding insert

--- examples/kafka-application-example/sink-job/SinkJob.py
+++ examples/kafka-application-example/sink-job/SinkJob.py
@@ -5,17 +5,41 @@ from edna.emit import KafkaEmit
 from edna.serializers import StringSerializer
 from edna.serializers import KafkaStringSerializer
 
+from edna.ingest.streaming import SimulatedIngest
+from edna.process.map import JsonToObject
+from edna.serializers.EmptySerializer import EmptyStringSerializer
+from edna.serializers.EmptySerializer import EmptyObjectSerializer
+
 def main():
+    list_of_inserts = ['Processed by Sink Job: ']
+
+    context2 = SimpleStreamingContext()
+
+    ingest2 = SimulatedIngest(serializer=EmptyStringSerializer, stream_list=list_of_inserts)
+
+    emit_serializer2 = KafkaStringSerializer()    # e.g. StringSerializer
+
+    process2 = BaseProcess()                     # e.g. BaseProcess
+    emit2 = KafkaEmit(emit_serializer2,
+        kafka_topic=context2.getVariable("export_key"),
+        bootstrap_server=contex2.getVariable("bootstrap_server"))           # e.g. KafkaEmit
+
+    context2.addIngest(ingest=ingest2)        # Registers the ingest primitive
+    context2.addProcess(process=process2)     # Registers the process primitive
+    context2.addEmit(emit=emit2)              # Registers the emit primitive
+
+    context2.execute()   # Executes  the context
+
     context = SimpleStreamingContext()     # Choose an appropriate context, such as SimpleStreamingContext
-    
+
     ingest_serializer = StringSerializer()  # e.g. KafkaStringSerializer
     emit_serializer = KafkaStringSerializer()    # e.g. StringSerializer
-    
-    ingest = KafkaIngest(ingest_serializer, 
+
+    ingest = KafkaIngest(ingest_serializer,
         kafka_topic=context.getVariable("import_key"),
         bootstrap_server=context.getVariable("bootstrap_server"))    # e.g. KafkaIngest
     process = BaseProcess()                     # e.g. BaseProcess
-    emit = KafkaEmit(emit_serializer, 
+    emit = KafkaEmit(emit_serializer,
         kafka_topic=context.getVariable("export_key"),
         bootstrap_server=context.getVariable("bootstrap_server"))           # e.g. KafkaEmit
 
